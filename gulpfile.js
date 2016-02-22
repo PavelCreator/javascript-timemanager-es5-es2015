780bdb0cb9c73113219fa08ffc58420cc98a2c37
PavelCreator <pavelcreator@gmail.com>
2016-02-22 23:45:13 +0200

    Gulp from es5 to es6

--- gulpfile.js
+++ gulpfile.js
@@ -19,22 +19,32 @@ var
   htmlmin = require('gulp-htmlmin'),
   removeHtmlComments = require('gulp-remove-html-comments'),
 
+  browserify = require('browserify'),
+  source = ('vinyl-source-stream'),
+
   config = {
     src: {
       css: 'src/scss/*.scss',
       img: 'src/img/*',
-      js: [
-        'src/js/hoisting.js',
-        'src/js/services/*.js',
-        'src/js/controllers/*.js',
-        'src/js/start.js'
+      es5: [
+        'src/es5/hoisting.js',
+        'src/es5/services/*.js',
+        'src/es5/controllers/*.js',
+        'src/es5/start.js'
+      ],
+      es6: [
+        'src/es6/hoisting.js.es6'/*,
+         'src/es6/services/!*.js',
+         'src/es6/controllers/!*.js',
+         'src/es6/start.js'*/
       ],
       html: 'index-src.html'
     },
     build: {
       css: 'build/css',
       img: 'build/img',
-      js: 'build/js',
+      es5: 'build/es5',
+      es6: 'build/es6',
       html: './'
     }
   }
@@ -53,19 +63,38 @@ gulp.task('build-css', function () {
 
 gulp.task('build-img', function () {
   gulp.src(config.src.img)
-    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(cache(imagemin({optimizationLevel: 5, progressive: true, interlaced: true})))
     .pipe(gulp.dest(config.build.img))
     .pipe(notify({message: 'Build Images task complete'}));
 });
 
-gulp.task('build-js', function () {
-  return gulp.src(config.src.js)
+gulp.task('build-es5', function () {
+  return gulp.src(config.src.es5)
     .pipe(concat('main.js'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-    .pipe(gulp.dest(config.build.js))
-    .pipe(notify({message: 'Build JS task complete'}));
+    .pipe(gulp.dest(config.build.es5))
+    .pipe(notify({message: 'Build ES5 task complete'}));
 });
+
+gulp.task("build-es6", function () {
+  return browserify(config.src.es6)
+    .transform("babelify")
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest(config.build.es6))
+    .pipe(notify({message: 'Build ES6 task complete'}));
+});
+
+/*gulp.task("build-es7", function () {
+  return gulp.src(config.src.es6)
+    .pipe(transform("babelify"))
+    .pipe(concat('main.js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest(config.build.es6))
+    .pipe(notify({message: 'Build ES6 task complete'}));
+});*/
 
 gulp.task('build-html', function () {
   return gulp.src(config.src.html)
@@ -76,15 +105,27 @@ gulp.task('build-html', function () {
     .pipe(notify({message: 'Build HTML task complete'}));
 });
 
-gulp.task('clean', function() {
-    return del(['build/css', 'build/js', 'build/img', 'index.html']);
+gulp.task('build-es6', function () {
+  return gulp.src(config.src.html)
+    .pipe(removeHtmlComments())
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest(config.build.html))
+    .pipe(notify({message: 'Build HTML task complete'}));
+});
+
+gulp.task('clean', function () {
+  return del(['build/css', 'build/es5', 'build/ed6', 'build/img', 'index.html']);
 });
 
 gulp.task('watch-css', function () {
   gulp.watch(config.src.css, ['build-css'])
 })
-gulp.task('watch-js', function () {
-  gulp.watch(config.src.js, ['build-js'])
+gulp.task('watch-es5', function () {
+  gulp.watch(config.src.es5, ['build-es5'])
+})
+gulp.task('watch-es6', function () {
+  gulp.watch(config.src.es6, ['build-es6'])
 })
 gulp.task('watch-img', function () {
   gulp.watch(config.src.img, ['build-img'])
@@ -93,7 +134,7 @@ gulp.task('watch-html', function () {
   gulp.watch(config.src.html, ['build-html'])
 })
 
-gulp.task('watch', ['watch-css', 'watch-js', 'watch-img', 'watch-html'])
-gulp.task('build', ['build-css', 'build-js', 'build-img', 'build-html'])
-gulp.task('default', ['build-html'])
+gulp.task('watch', ['watch-css', 'watch-es5', 'watch-img', 'watch-html'])
+gulp.task('build', ['build-css', 'build-es5', 'build-img', 'build-html'])
+gulp.task('default', ['build-es6'])
 /*npm install --save-dev packName*/
