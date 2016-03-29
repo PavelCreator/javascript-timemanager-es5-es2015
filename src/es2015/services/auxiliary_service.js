e70ef62a1e422cfba91c7d29885660b62c36f335
PavelCreator <pavelcreator@gmail.com>
2016-03-29 18:54:11 +0300

    ES2015. Part 8.1

--- src/es2015/services/auxiliary_service.js
+++ src/es2015/services/auxiliary_service.js
@@ -1,21 +1,21 @@
-logger = function () {
+logger = () => {
   console.log(`time.h = ${data.time.h}`);
   console.log(`time.m = ${data.time.m}`);
   console.log(`time.s = ${data.time.s}`);
   console.log(`timeInSec = ${data.timeInSec}`);
 };
 classFnc = {
-  add: function (o, c) {
+  add(o, c) {
     let re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g")
     if (re.test(o.className)) return
     o.className = (`${o.className} ${c}`).replace(/\s+/g, " ").replace(/(^ | $)/g, "")
   },
-  remove: function (o, c) {
+  remove(o, c) {
     let re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g")
     o.className = o.className.replace(re, "$1").replace(/\s+/g, " ").replace(/(^ | $)/g, "")
   }
 };
-addEvent = function (object, type, callback) {
+addEvent = (object, type, callback) => {
   if (object == null || typeof(object) == 'undefined') return;
   if (object.addEventListener) {
     object.addEventListener(type, callback, false);
@@ -25,7 +25,7 @@ addEvent = function (object, type, callback) {
     object[`on${type}`] = callback;
   }
 };
-addZero = function (i) {
+addZero = (i) => {
   if (i < 10) {
     i = `0${i}`;
   }
